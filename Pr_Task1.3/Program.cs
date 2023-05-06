76b2e6c0a14bd9b4cb66ba33d311fd11610269f6
RaGaLiK <RaGaLiK@DESKTOP-58NBP1E>
2023-05-06 17:07:34 +0400

    Добавьте файлы проекта.

---
+++ Pr_Task1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr_Task1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string> { "bella", "label", "roller" };
+            HashSet<char> commonChars = new HashSet<char>(strings[0]);
+
+            foreach (string str in strings.Skip(1))
+            {
+                HashSet<char> chars = new HashSet<char>(str);
+                commonChars.IntersectWith(chars);
+            }
+            Console.WriteLine(string.Join("", commonChars));
+            Console.ReadLine();
+        }
+    }
+}
